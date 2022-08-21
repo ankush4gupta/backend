fddcb5e43ea7fe4827068c9264e6fd4aa9500ff4
Ankush Gupta <87088148+ankush4gupta@users.noreply.github.com>
2022-08-21 15:40:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.use(express.json())
 const { register, login } = require("./controllers/Authentication");
 const companycontroller = require("./controllers/company.controller")
 const usercontroller = require("./controllers/usercontroller");
-// login and registration
+
 app.post("/register", register);
 app.post("/login", login);
 app.use("/company", companycontroller)
